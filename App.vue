a4e10b59ec07a049d4bc8231d4252074feea1bf6
difffffft <672228275@qq.com>
2022-09-29 19:20:57 +0800

    change

--- App.vue
+++ App.vue
@@ -1,14 +1,17 @@
 <script>
-	export default {
-		onLaunch: async function() {},
-		onShow: async function() {},
-		onHide: async function() {}
-	}
+import versionUpdateCheck from '@/plugin/version.plugin';
+export default {
+	onLaunch: async function() {
+		versionUpdateCheck.install();
+	},
+	onShow: async function() {},
+	onHide: async function() {}
+};
 </script>
 
 <style lang="scss">
-	//uView的组件公共样式
-	@import "@/uni_modules/uview-ui/index.scss";
-	//项目的公共样式
-	@import '~@/style/app.module.scss';
+//uView的组件公共样式
+@import '@/uni_modules/uview-ui/index.scss';
+//项目的公共样式
+@import '~@/style/app.module.scss';
 </style>
